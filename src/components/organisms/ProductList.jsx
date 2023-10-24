19a4b44e50383c8a07721486515c6b18b394e87e
TioNovriadiPutra <tionvriadi@gmail.com>
2023-10-24 18:52:13 +0700

    style: fixing aspect ratio for image modal

--- src/components/organisms/ProductList.jsx
+++ src/components/organisms/ProductList.jsx
@@ -1,15 +1,18 @@
-import { StyleSheet, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import React, { useEffect, useRef } from "react";
 import ProductImageGallery from "@components/molecules/ProductImageGallery";
 import ProductScrollButton from "@components/atoms/ProductScrollButton";
 import { useRecoilState, useRecoilValue } from "recoil";
-import { listIndexState, productState } from "@store/productState";
+import { listIndexState, productState, showMobileModalImageState } from "@store/productState";
 import useResponsive from "@hooks/useResponsive";
 import { colors } from "@themes/colors";
+import { fonts } from "@themes/fonts";
+import ImageModal from "@components/templates/ImageModal";
 
 const ProductList = () => {
   const [listIndex, setListIndex] = useRecoilState(listIndexState);
   const product = useRecoilValue(productState);
+  const showMobileModal = useRecoilValue(showMobileModalImageState);
 
   const { isTabletOrMobileDevice } = useResponsive();
 
@@ -34,8 +37,20 @@ const ProductList = () => {
   return (
     <View style={[styles.container, isTabletOrMobileDevice ? styles.containerMobile : styles.containerWeb]}>
       {isTabletOrMobileDevice ? null : listIndex > 0 && <ProductScrollButton func="prev" onPress={handlePrev} />}
+
       <ProductImageGallery listRef={listRef} />
+
       {isTabletOrMobileDevice ? null : listIndex < product.data.length - 1 && <ProductScrollButton onPress={handleNext} />}
+
+      {isTabletOrMobileDevice && (
+        <View style={styles.descContainer}>
+          <Image source={require("@assets/images/previousBlack.png")} style={styles.icon} />
+          <Text style={styles.desc}>Swipe</Text>
+          <Image source={require("@assets/images/nextBlack.png")} style={styles.icon} />
+        </View>
+      )}
+
+      {isTabletOrMobileDevice && <ImageModal visible={showMobileModal} />}
     </View>
   );
 };
@@ -61,5 +76,22 @@ const styles = StyleSheet.create({
       height: 0,
     },
     shadowRadius: 4,
+  },
+  descContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 30,
+    marginTop: 20,
+    marginBottom: 15,
+  },
+  icon: {
+    width: 15,
+    height: 15,
+  },
+  desc: {
+    fontFamily: fonts.PopRegular,
+    fontSize: 16,
+    color: colors.Grey,
   },
 });
